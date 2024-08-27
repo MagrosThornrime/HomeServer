1368ecf5d7886207dbfdf5819d9da973da6b84db
MagrosThornrime <magros.thornrime@gmail.com>
2024-08-28 01:10:07 +0200

    Make '/sensor_entry' route parse number data

--- SensorsDatabase.py
+++ SensorsDatabase.py
@@ -22,12 +22,13 @@ def get_entries() -> list[dict]:
     
 def add_entry(sensor_entries: list[dict], sensor: str, data: dict):
     current_time = datetime.now().isoformat(sep=" ", timespec="minutes")
-    entry = {key: value for key, value in data.items() if key != "Czujnik"}
+    entry = {key: float(value) for key, value in data.items() if key != "Czujnik"}
     entry["Czujnik"] = sensor
     entry["Czas"] = current_time
     sensor_entries.append(entry)
     if len(sensor_entries) > MAX_ENTRIES:
         del sensor_entries[0]
+        # TODO what if I change the limits and my file is too big?
     with open(SENSORS_FILE, "w") as json_file:
         json.dump(sensor_entries, json_file, indent=2)
 
