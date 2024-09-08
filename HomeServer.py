5e088424644a8cf379611a9244b9478a904d6c8f
MagrosThornrime <magros.thornrime@gmail.com>
2024-09-08 03:13:30 +0200

    Add sensors data validation. If there are too many entries in the database, delete excess ones

--- HomeServer.py
+++ HomeServer.py
@@ -1,32 +1,41 @@
+﻿from sqlite3 import DatabaseError
 from flask import Flask, render_template, request
 
 import SensorsDatabase as sensors_db
 
 app = Flask(__name__)
-sensor_entries = sensors_db.get_entries()
 
 @app.route("/sensor_entry", methods=["POST"])
 def add_sensor_data():
     try:
         sensor = request.form["Czujnik"]
     except KeyError:
-        print("Sensor's name not found")
-        return ""
+        return render_template("error.html", error_info="Żądanie nie zawiera nazwy czujnika")
+    sensor_entries = sensors_db.get_entries()
     data = request.form.to_dict()
     sensors_db.add_entry(sensor_entries, sensor, data)
     return ""
     
 @app.route("/sensors")
 def sensors():
+    try:
+        sensor_entries = sensors_db.get_entries()
+    except sensors_db.SensorError as error:
+        return render_template("error.html", error_info=str(error))
     latest_entries = sensors_db.get_latest(sensor_entries)
     return render_template("sensors.html", sensor_entries=latest_entries)
 
 @app.route("/sensors/<sensor>")
 def sensor_detailed(sensor: str):
-    # TODO: what if somebody uses the url but there are no entries for the sensor?
+    try:
+        sensor_entries = sensors_db.get_entries()
+    except sensors_db.SensorError as error:
+        return render_template("error.html", error_info=str(error))
     entries = sensors_db.get_sensor(sensor_entries, sensor)
+    if not entries:
+        return render_template("error.html", error_info="Baza danych nie zawiera wpisów z tego czujnika")
     sensors_db.create_plots(entries, sensor)
-    keys = sensors_db.get_keys(sensor_entries)
+    keys = sensors_db.get_keys(entries[0])
     return render_template("sensors_detailed.html", sensor=sensor, keys=keys)
 
 @app.route("/guide")
--- SensorsDatabase.py
+++ SensorsDatabase.py
@@ -1,4 +1,4 @@
-import json
+﻿import json
 from datetime import datetime
 
 import pandas as pd
@@ -10,15 +10,54 @@ import matplotlib.pyplot as plt
 SENSORS_FILE = "data/sensors.json"
 MAX_ENTRIES = 100
 
+class SensorError(Exception):
+    pass
+
+def trim_entries(sensor_entries: list[dict]):
+    if len(sensor_entries) > MAX_ENTRIES:
+        excess_entries = len(sensor_entries) - MAX_ENTRIES
+        del sensor_entries[:excess_entries]
+        
+def get_keys(entry: dict):
+    return [key for key in entry.keys() if key not in {"Czas", "Czujnik"}]
+
+def validate(sensor_entries: list[dict]):
+    used_keys = {}
+
+    for entry in sensor_entries:        
+        # check if sensor name is valid
+        if "Czujnik" not in entry:
+            raise SensorError("Wpis z bazy nie zawiera nazwy czujnika")
+        sensor = entry["Czujnik"]
+        if not isinstance(sensor, str):
+            raise SensorError("Wpis z bazy posiada niepoprawną nazwę czujnika")
+
+        # check if saved time is valid
+        if "Czas" not in entry:
+            raise SensorError("Wpis z bazy nie posiada daty i godziny")
+        time = entry["Czas"]
+        try:
+            datetime.fromisoformat(time)
+        except ValueError:
+            raise SensorError("Wpis z bazy posiada niepoprawną datę")
+        
+        # check if entry is similiar to all others from the sensor
+        keys = get_keys(entry)
+        if sensor in used_keys and set(keys) != used_keys[sensor]:
+            raise SensorError(f"Wpisy z bazy dotyczące czujników '{sensor}' mają różne parametry")
+        else:
+            used_keys[sensor] = set(keys)
+
 def get_entries() -> list[dict]:
     sensor_entries = []
     try:
         with open(SENSORS_FILE, "r") as json_file:
             sensor_entries = json.load(json_file)
+            validate(sensor_entries)
+            trim_entries(sensor_entries)
     except FileNotFoundError:
         open(SENSORS_FILE, "w").close()
-    finally:
-        return sensor_entries
+    return sensor_entries
     
 def add_entry(sensor_entries: list[dict], sensor: str, data: dict):
     current_time = datetime.now().isoformat(sep=" ", timespec="minutes")
@@ -26,13 +65,17 @@ def add_entry(sensor_entries: list[dict], sensor: str, data: dict):
     entry["Czujnik"] = sensor
     entry["Czas"] = current_time
     sensor_entries.append(entry)
-    if len(sensor_entries) > MAX_ENTRIES:
-        del sensor_entries[0]
-        # TODO what if I change the limits and my file is too big?
+    try:
+        validate(sensor_entries)
+    except SensorError:
+        del sensor_entries[-1]
+        return
+    trim_entries(sensor_entries)
     with open(SENSORS_FILE, "w") as json_file:
         json.dump(sensor_entries, json_file, indent=2)
 
 def get_latest(sensor_entries: list[dict]) -> list[dict]:
+    validate(sensor_entries)
     entries_found = {}
     for entry in sensor_entries[::-1]:
         sensor = entry["Czujnik"]
@@ -43,9 +86,11 @@ def get_latest(sensor_entries: list[dict]) -> list[dict]:
     return entries_found
 
 def get_sensor(sensor_entries: list[dict], sensor: str) -> list[dict]:
+    validate(sensor_entries)
     return [entry for entry in sensor_entries if entry["Czujnik"] == sensor]
 
 def create_plots(sensor_entries: list[dict], sensor: str):
+    validate(sensor_entries)
     measurements = {}
     for entry in sensor_entries:
         for key, value in entry.items():
@@ -66,9 +111,3 @@ def create_plots(sensor_entries: list[dict], sensor: str):
         plot.set(xlabel="Liczba godzin od pomiaru", ylabel=series_name)
         plt.savefig(f"static/temp/{sensor}_{series_name}.png")
         plt.clf()
-
-def get_keys(sensor_entries: list[dict]):
-    example = sensor_entries[0]
-    return [key for key in example.keys() if key not in {"Czas", "Czujnik"}]
-
-# TODO: add data validation
